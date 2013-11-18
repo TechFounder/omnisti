ad79d86548f40ef75dfd0f6f30783775356d808c
TechFounder <techfounder@outlook.com>
2013-11-18 00:16:50 -0500

    Make initial STI models admin and editor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Omnisti::Application.routes.draw do
   devise_for :users
+  namespace :admin do
+    devise_for :users
+  end  
   get 'about' => "static_pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
